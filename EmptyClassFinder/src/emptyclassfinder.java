b767d4ba7aded00381777923ee5f3919d88405c6
WinnMoo <winnmoo@gmail.com>
2017-07-13 20:42:57 -0700

    Created project files

---
+++ EmptyClassFinder/src/emptyclassfinder.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Winn
+ */
+
+
+
+public class emptyclassfinder {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
